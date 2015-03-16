82b03e0fe90927c47c7eab09a9b08bc77155052c
Matthew Miller <matthewmiller210@gmail.com>
2015-03-16 11:10:44 -0600

    More Testing
    Still do not ask...

--- type/A4_embedded/certificate.php
+++ type/A4_embedded/certificate.php
@@ -94,6 +94,7 @@ certificate_print_text($pdf, $x, $y, 'C', $fontsans, '', 30, get_string('title',
 $pdf->SetTextColor(0, 0, 0);
 certificate_print_text($pdf, $x, $y + 20, 'C', $fontserif, '', 20, get_string('certify', 'certificate'));
 certificate_print_text($pdf, $x, $y + 36, 'C', $fontsans, '', 30, fullname($USER));
+certificate_print_text($pdf, $x, $y + 36, 'C', $fontsans, '', 30, fullname($USER));
 certificate_print_text($pdf, $x, $y + 55, 'C', $fontsans, '', 20, get_string('statement', 'certificate'));
 certificate_print_text($pdf, $x, $y + 72, 'C', $fontsans, '', 20, format_string($course->fullname));
 certificate_print_text($pdf, $x, $y + 92, 'C', $fontsans, '', 14,  certificate_get_date($certificate, $certrecord, $course));
--- type/letter_embedded/certificate.php
+++ type/letter_embedded/certificate.php
@@ -94,6 +94,7 @@ certificate_print_text($pdf, $x, $y, 'C', $fontsans, '', 30, get_string('title',
 $pdf->SetTextColor(0, 0, 0);
 certificate_print_text($pdf, $x, $y + 55, 'C', $fontserif, '', 20, get_string('certify', 'certificate'));
 certificate_print_text($pdf, $x, $y + 105, 'C', $fontserif, '', 30, fullname($USER));
+certificate_print_text($pdf, $x, $y + 105, 'C', $fontserif, '', 30, fullname($USER));
 certificate_print_text($pdf, $x, $y + 155, 'C', $fontserif, '', 20, get_string('statement', 'certificate'));
 certificate_print_text($pdf, $x, $y + 205, 'C', $fontserif, '', 20, format_string($course->fullname));
 certificate_print_text($pdf, $x, $y + 255, 'C', $fontserif, '', 14, certificate_get_date($certificate, $certrecord, $course));
